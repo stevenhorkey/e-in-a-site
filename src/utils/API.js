843f15f0f75b2d0e94c00a307794fa52963f1bf2
stevenhorkey <code@stevenhorkey.com>
2018-10-09 18:14:04 -0700

    api fix

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-var md5 = require('md5');
+// var md5 = require('md5');
 
 // All api routes are defined here centrally for ease of use.
-// const url = 'https://api.memeandmeaning.com/wp-json/wp/v2/';
-// const url = 'https://api.memeandmeaning.com/wp-json/wp/v2/';
-const url = 'http://54.202.248.102/wp-json/wp/v2/';
-const mcListId = 'd25ec94b8f';
+const url = 'https://api.memeandmeaning.com/wp-json/wp/v2/';
+// const mcListId = 'd25ec94b8f';
 
 
 export default {
@@ -14,5 +12,5 @@ export default {
   getPages: () => axios.get(url + 'pages'),
   getPost: slug => axios.get(url + 'posts?slug=' + slug),
   getPage: () => axios.get(url + 'page'),
-  mcAddSubscriber: (data) => axios.post('https://memeandmeaning.us16.list-manage.com/subscribe/post?u=4c73c4e387b1f2b219c1f2af6&amp;id=d25ec94b8f', data)
+  // mcAddSubscriber: (data) => axios.post('https://memeandmeaning.us16.list-manage.com/subscribe/post?u=4c73c4e387b1f2b219c1f2af6&amp;id=d25ec94b8f', data)
 };
